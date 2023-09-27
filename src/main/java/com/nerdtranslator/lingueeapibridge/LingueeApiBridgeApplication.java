a8f03a48e5c13010a68b2a1402c9bb72d1a8478b
Artem Prokhorov <electron3djavadev@gmail.com>
2023-09-27 10:22:21 +0300

    initial commit

---
+++ src/main/java/com/nerdtranslator/lingueeapibridge/LingueeApiBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.nerdtranslator.lingueeapibridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingueeApiBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingueeApiBridgeApplication.class, args);
+	}
+
+}
