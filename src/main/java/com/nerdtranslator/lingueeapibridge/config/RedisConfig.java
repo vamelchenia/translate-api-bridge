123cd2c4fa0f26a32d544effb1c001b9e7cf08e3
Artem Prokhorov <electron3djavadev@gmail.com>
2023-09-28 20:40:30 +0300

    fixed redis config, so now application can start and running without any errors, but it isn't do anything because of apiService isn't ready yet

--- src/main/java/com/nerdtranslator/lingueeapibridge/config/RedisConfig.java
+++ src/main/java/com/nerdtranslator/lingueeapibridge/config/RedisConfig.java
@@ -12,12 +12,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
-
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
-    @Value("${spring.redis.password}")
+    @Value("${spring.data.redis.password}")
     private String redisPassword;
 
     @Bean
