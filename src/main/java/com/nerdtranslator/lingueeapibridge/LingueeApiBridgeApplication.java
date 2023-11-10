07f11e1e5df112c7afc74163e73d22fbd273e583
Evgeny Savonkin <evgenysavonkin@gmail.com>
2023-11-10 17:57:14 +0300

    Remove redis support

--- src/main/java/com/nerdtranslator/lingueeapibridge/LingueeApiBridgeApplication.java
+++ src/main/java/com/nerdtranslator/lingueeapibridge/LingueeApiBridgeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LingueeApiBridgeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LingueeApiBridgeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LingueeApiBridgeApplication.class, args);
+    }
 
 }
--- src/main/java/com/nerdtranslator/lingueeapibridge/service/RedisCacheService.java
+++
@@ -1,6 +0,0 @@
-package com.nerdtranslator.lingueeapibridge.service;
-
-public interface RedisCacheService {
-    String getCachedTranslation(String originalText, String from, String to);
-    void cacheTranslation(String originalText, String from, String to, String response);
-}
