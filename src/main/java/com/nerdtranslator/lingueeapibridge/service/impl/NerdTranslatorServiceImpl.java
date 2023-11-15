3b12dfe13b2ea6fbfce3dc1f74f77f901d523db3
Artem Prokhorov <electron3djavadev@gmail.com>
2023-11-16 00:02:32 +0300

    Fixed warnings

--- src/main/java/com/nerdtranslator/lingueeapibridge/service/impl/NerdTranslatorServiceImpl.java
+++ src/main/java/com/nerdtranslator/lingueeapibridge/service/impl/NerdTranslatorServiceImpl.java
@@ -1,19 +1,21 @@
 package com.nerdtranslator.lingueeapibridge.service.impl;
 
-import com.nerdtranslator.lingueeapibridge.service.TextToSpeechService;
 import com.nerdtranslator.lingueeapibridge.service.TranslationApiService;
 import com.nerdtranslator.lingueeapibridge.service.NerdTranslatorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class NerdTranslatorServiceImpl implements NerdTranslatorService {
     private final TranslationApiService translationApiService;
-    private final TextToSpeechService textToSpeechService;
 
     @Override
     public String getTranslation(String originalText, String originalLanguage, String targetLanguage) {
+        List<String> translations =
+                translationApiService.fetchDataFromApi("", originalText, originalLanguage, targetLanguage);
         return null;
     }
 }
