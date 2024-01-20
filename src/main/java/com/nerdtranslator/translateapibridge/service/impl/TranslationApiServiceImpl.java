e9c96797c72c22edb1a3352ccbc586d8356916ab
Artem Prokhorov <electron3djavadev@gmail.com>
2024-01-20 18:26:39 +0300

    deleted unused method

--- src/main/java/com/nerdtranslator/translateapibridge/service/impl/TranslationApiServiceImpl.java
+++ src/main/java/com/nerdtranslator/translateapibridge/service/impl/TranslationApiServiceImpl.java
@@ -9,8 +9,6 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @PropertySource("classpath:sensitive.properties")
@@ -18,31 +16,6 @@ import java.util.stream.Collectors;
 public class TranslationApiServiceImpl implements TranslationApiService {
     private final CredentialsProviderFactory credentialsProviderFactory;
     private final Environment env;
-
-    @Override
-    public List<String> getTranslationsFromApi(String originalText, String originalLanguage, String targetLanguage) {
-        try (TranslationServiceClient client = TranslationServiceClient.create(
-                TranslationServiceSettings
-                        .newBuilder()
-                        .setCredentialsProvider(credentialsProviderFactory.getCredentialsProvider())
-                        .build())) {
-            LocationName parent = LocationName.of(env.getProperty("PROJECT_ID"), "global");
-            TranslateTextRequest request = TranslateTextRequest.newBuilder()
-                    .setParent(parent.toString())
-                    .setMimeType("text/plain")
-                    .setTargetLanguageCode(targetLanguage)
-                    .addContents(originalText)
-                    .build();
-
-            TranslateTextResponse response = client.translateText(request);
-            return response.getTranslationsList()
-                    .stream()
-                    .map(Translation::getTranslatedText)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
 
     @Override
     public String getSingleTranslationFromApi(String originalText, String originalLanguage, String targetLanguage) {
